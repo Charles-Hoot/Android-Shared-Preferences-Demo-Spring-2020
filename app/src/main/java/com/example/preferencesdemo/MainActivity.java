f7e0e07e1cf71e073ba2f24449eb1ce2cf73cb72
Charles Hoot <hoot@nwmissouri.edu>
2020-03-31 02:46:11 -0500

    set text in TV

--- app/src/main/java/com/example/preferencesdemo/MainActivity.java
+++ app/src/main/java/com/example/preferencesdemo/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.preferencesdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public String nickname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView resultTV = findViewById(R.id.resultTV);
+        resultTV.setText("Nickname is:"+nickname);
     }
 }
